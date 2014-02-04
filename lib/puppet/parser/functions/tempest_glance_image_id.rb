2c23b6a9044736225b966e28e8ea8f7e5c48f6ee
Damian Szeluga <dszeluga@mirantis.com>
2014-02-04 15:23:04 +0100

    Adding timeout

--- lib/puppet/parser/functions/tempest_glance_image_id.rb
+++ lib/puppet/parser/functions/tempest_glance_image_id.rb
@@ -18,13 +18,13 @@ auth_password
       raise(Puppet::ParseError, "tempest_glance_id(): Wrong number of arguments "+
         "given #{arguments.size} for 1")
     end
-    
+
     require 'json'
     require 'net/http'
     require 'net/https'
     require 'openssl'
     require 'uri'
-    
+
     image_name = arguments[0]
     image_id = nil
     auth_keystone_uri = arguments[1]
@@ -32,7 +32,7 @@ auth_password
     auth_username = arguments[3]
     auth_password = arguments[4]
     os_services = {}
-    
+
     keystone_auth_data = JSON.generate({
       "auth" =>  { 
         "tenantName" => auth_tenant,
@@ -42,7 +42,7 @@ auth_password
         }
       }
     })
-    
+
     keystone_uri = URI.parse(auth_keystone_uri)
     keystone_path = keystone_uri.path.chomp('/') + '/tokens'
     keystone_connection = Net::HTTP.new(keystone_uri.host, keystone_uri.port)
@@ -50,36 +50,41 @@ auth_password
       keystone_connection.use_ssl = true
       keystone_connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
     end
-    
-    keystone_response = keystone_connection.post(keystone_path, keystone_auth_data, {'Content-Type' => 'application/json'});
-    if (keystone_response.code =~ /^20./)
-      keystone_response_json = JSON.parse(keystone_response.body)
-      keystone_response_json['access']['serviceCatalog'].each do |service|
-        os_services[service['type']] = service['endpoints'][0]['internalURL']
-      end
-
-      if os_services.has_key?('image')
-
-        glance_uri = URI.parse(os_services['image'])
-        glance_connection = Net::HTTP.new(glance_uri.host, glance_uri.port)
-        if glance_uri.scheme =='https'
-          glance_connection.use_ssl = true
-          glance_connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    begin
+      keystone_connection.open_timeout = 2
+      keystone_connection.read_timeout = 2
+      keystone_response = keystone_connection.post(keystone_path, keystone_auth_data, {'Content-Type' => 'application/json'});
+      if (keystone_response.code =~ /^20./)
+        keystone_response_json = JSON.parse(keystone_response.body)
+        keystone_response_json['access']['serviceCatalog'].each do |service|
+          os_services[service['type']] = service['endpoints'][0]['internalURL']
         end
-        
-        glance_path = glance_uri.path.chomp('/') + '/v1/images'
-        glance_response = glance_connection.get(glance_path, {'X-Auth-Token' => keystone_response_json['access']['token']['id'], 'Content-Type' => 'application/json'});
-        if (glance_response.code =~ /^20./)
-          glance_response_json = JSON.parse(glance_response.body)
-          glance_response_json['images'].each do |image|
-            if image_name == image['name']
-              image_id = image['id']
+
+        if os_services.has_key?('image')
+
+          glance_uri = URI.parse(os_services['image'])
+          glance_connection = Net::HTTP.new(glance_uri.host, glance_uri.port)
+          if glance_uri.scheme =='https'
+            glance_connection.use_ssl = true
+            glance_connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          end
+
+          glance_path = glance_uri.path.chomp('/') + '/v1/images'
+          glance_response = glance_connection.get(glance_path, {'X-Auth-Token' => keystone_response_json['access']['token']['id'], 'Content-Type' => 'application/json'});
+          if (glance_response.code =~ /^20./)
+            glance_response_json = JSON.parse(glance_response.body)
+            glance_response_json['images'].each do |image|
+              if image_name == image['name']
+                image_id = image['id']
+              end
             end
           end
         end
       end
+
+    rescue Timeout::Error => e
+      Puppet.info "Timeout while contacting keystone: #{e}"
     end
-    
     return image_id
     
   end
